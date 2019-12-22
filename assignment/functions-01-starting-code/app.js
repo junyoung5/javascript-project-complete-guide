dba803021399ba36f76fce6b0dcd6982edef61f5
JunyoungOh <jun.ohmazing@gmail.com>
2019-12-23 01:37:38 +0900

    rockPaperScissors

--- assignment/functions-01-starting-code/app.js
+++ assignment/functions-01-starting-code/app.js
@@ -1,7 +1,35 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-function startGame() {
-    console.log('Game is starting...');
+const ROCK = 'ROCK';
+const PAPER = 'PAPER';
+const SCISSORS = 'SCISSORS';
+const DEFAULT_USER_CHOICE = ROCK;
+
+let gameIsRunning = false;
+
+const getPlayerChoice = function(){
+    const selection = prompt(`${ROCK}, ${PAPER} or ${SCISSORS}?`).toUpperCase();
+
+    if(
+        selection !== ROCK &&
+        selection !== PAPER &&
+        selection !== SCISSORS
+    ) {
+        alert(`invalid choice! we choose ${DEFAULT_USER_CHOICE}`);
+        return DEFAULT_USER_CHOICE;
+    }
+    return selection;
+
 }
 
-startGameBtn.addEventListener('click', startGame);
+
+// prevent memory leak. it needs to be run when it 
+startGameBtn.addEventListener('click', function () {
+    if (gameIsRunning){
+        return ;
+    }
+    gameIsRunning = true;
+    console.log('Game is starting...');
+    const playerSelection = getPlayerChoice();
+    console.log(playerSelection);
+});
